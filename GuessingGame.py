54bc9c1ecd7b9e5fd147d9f1a14c330cde731a73
Pyro-Impact <77907654+Pyro-Impact@users.noreply.github.com>
2021-02-10 22:03:16 -0800

    Added more features and a for loop to practice for loops

--- GuessingGame.py
+++ GuessingGame.py
@@ -7,26 +7,44 @@ def random_num_generator(random_argument, user_num):
         return "Your guess is too low of a value!"
     elif random_argument < user_num:
         return "Your guess is too high of a value!"
-    else:
+    elif random_argument == user_num:
         return "Wow, you guessed the correct value!"
 
+
+guesses = 0
+list_of_guesses = 0
+usernuminput = 0
 
 while True:
     print("You have 3 guesses.")
     random_num = random.choice(range(21))
-    guesses = 0
+
     while True:
-        try:
-            usernuminput = int(input("Please enter a number (0 to 20): "))
-            print(random_num_generator(random_num, usernuminput))
-            if usernuminput == random_num:
+        while True:
+            try:
+                usernuminput = int(input("Please enter a number (0 to 20): "))
+            except ValueError:
+                print("Please enter a valid number.")
+
+            if usernuminput not in range(0, 21):
+                print("Please enter a number within the range of 0 to 20...")
                 break
-        except ValueError:
-            print("Please enter a valid number.")
+            elif usernuminput == random_num:
+                break
+            else:
+                print(random_num_generator(random_num, usernuminput))
+                break
+
         guesses += 1
-        if guesses == 3:
+        for i in range(1, guesses + 1):
+            list_of_guesses = i
+
+        if list_of_guesses == 3:
             print("You've ran out of guesses.")
             break
+        else:
+            continue
+
     if confirm_restart() == "yes":
         continue
     else:
